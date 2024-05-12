e3dca76133b3c51fa172c28e509f15a453b73f4b
Shane <6071159+smashedr@users.noreply.github.com>
2024-05-12 16:51:47 -0700

    Fix Tabs

--- src/js/exports.js
+++ src/js/exports.js
@@ -83,11 +83,9 @@ export async function openAllBookmarks() {
     if (!bookmarks?.length) {
         chrome.runtime.openOptionsPage()
     }
-    let active = true
     for (const url of bookmarks) {
         console.debug(`url: ${url}`)
-        await chrome.tabs.create({ active, url })
-        active = false
+        chrome.tabs.create({ active: true, url }).then()
     }
     window.close()
 }
